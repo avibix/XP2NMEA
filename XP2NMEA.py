c4ec314fe235cb2d7468b0255fbd577e2cea71d4
avibix <165882836+avibix@users.noreply.github.com>
2025-06-19 19:24:43 +0300

    Add files via upload

--- XP2NMEA.py
+++ XP2NMEA.py
@@ -5,17 +5,13 @@ import os
 host = socket.gethostname()
 local_ip = socket.gethostbyname(host)
 
-data_size = 0
-start_time = time.time()
-last_update = time.time()
-
 while True:
     try:
-        #UDP
+        # UDP
         UDP_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
         UDP_socket.bind(('localhost', 16969))
 
-        #TCP
+        # TCP
         server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         server_socket.bind(('', 10110))
         server_socket.listen(1)
@@ -24,31 +20,37 @@ while True:
         client_socket, client_address = server_socket.accept()
         print(f"Connection has been established with {client_address}")
 
+        interval_data_size = 0
+        last_update = time.time()
+        start_time = time.time() 
+
         while True:
             conn, addr = UDP_socket.recvfrom(10245)
-            data_size += len(conn)
+            
+            client_socket.sendall(conn)
+            interval_data_size += len(conn)
+
             current_time = time.time()
             elapsed_time = current_time - start_time
             update_interval = current_time - last_update
 
-            if elapsed_time > 0 and update_interval >= 1:  # Обновляем каждую секунду
-                speed = (data_size / 1024) / elapsed_time  # kB/s
+            if update_interval >= 1: 
+                speed = (interval_data_size / 1024) / update_interval
+                interval_data_size = 0
+                last_update = current_time
+
                 elapsed_seconds = int(elapsed_time)
                 hours = elapsed_seconds // 3600
                 minutes = (elapsed_seconds % 3600) // 60
                 seconds = elapsed_seconds % 60
                 elapsed_str = f"{hours:02d}:{minutes:02d}:{seconds:02d}"
                 print(f"\rSpeed rate: {speed:.2f} kB/s   |   Elapsed: {elapsed_str}", end="")
-                last_update = current_time
-
-            client_socket.sendall(conn)
 
     except:
-        # Закрываем только UDP сокет
         try:
             UDP_socket.close()
         except:
             pass
-        # Очищаем консоль
+        # clear console
         os.system('cls' if os.name == 'nt' else 'clear')
         continue
